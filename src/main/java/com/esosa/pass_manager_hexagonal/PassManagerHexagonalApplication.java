64cbb382301ec90fa54d94f40e67b11b7da13079
EzeSosa <ezequielsosa423@gmail.com>
2024-08-18 23:38:13 -0300

    init

---
+++ src/main/java/com/esosa/pass_manager_hexagonal/PassManagerHexagonalApplication.java
@@ -0,0 +1,11 @@
+package com.esosa.pass_manager_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassManagerHexagonalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PassManagerHexagonalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/esosa/pass_manager_hexagonal/PassManagerHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esosa.pass_manager_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassManagerHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
