4216bdc7e6e95ecd153543b94343f7e1f0156b04
EzeSosa <ezequielsosa423@gmail.com>
2024-08-21 00:15:28 -0300

    feat: handle response status exception

--- src/main/java/com/esosa/pass_manager_hexagonal/infrastructure/exceptions/CustomExceptionManager.java
+++ src/main/java/com/esosa/pass_manager_hexagonal/infrastructure/exceptions/CustomExceptionManager.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.NoSuchElementException;
 
@@ -12,6 +13,11 @@ public class CustomExceptionManager {
 
     @ExceptionHandler(NoSuchElementException.class) @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionPayload handleNoSuchElementException(NoSuchElementException exception) {
+        return new ExceptionPayload( exception.getMessage(), HttpStatus.BAD_REQUEST.value() );
+    }
+
+    @ExceptionHandler(ResponseStatusException.class) @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionPayload handleResponseStatusException(ResponseStatusException exception) {
         return new ExceptionPayload( exception.getMessage(), HttpStatus.BAD_REQUEST.value() );
     }
 
