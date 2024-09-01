4fb5d6b2c8b48bdf20c83c972dc85d3691cd74fb
EzeSosa <ezequielsosa423@gmail.com>
2024-09-01 00:32:35 -0300

    del: builder from models

--- src/main/java/com/esosa/pass_manager_hexagonal/domain/model/Password.java
+++ src/main/java/com/esosa/pass_manager_hexagonal/domain/model/Password.java
@@ -57,49 +57,4 @@ public class Password {
         this.user = user;
     }
 
-    public static PasswordBuilder builder() {
-        return new PasswordBuilder();
-    }
-
-    public static class PasswordBuilder {
-
-        private UUID id = UUID.randomUUID();
-        private String name;
-        private String password;
-        private LocalDate createdAt = LocalDate.now();
-        private User user;
-
-        private PasswordBuilder() {}
-
-        public PasswordBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public PasswordBuilder id(UUID id) {
-            this.id = id;
-            return this;
-        }
-
-        public PasswordBuilder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public PasswordBuilder createdAt(LocalDate createdAt) {
-            this.createdAt = createdAt;
-            return this;
-        }
-
-        public PasswordBuilder user(User user) {
-            this.user = user;
-            return this;
-        }
-
-        public Password build() {
-            return new Password(id, name, password, createdAt, user);
-        }
-
-    }
-
 }
